413bce8d47b2ff0b086c3b0342bbf5647aa08ee9
名倉　由華 <yuru1322@gmail.com>
2026-07-19 10:18:31 +0900

    Rename kadai_020 to kadai_021#	renamed:    kadai_020/event.html -> kadai_021/event.html

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text')
 btn.addEventListener('click', () => {
-  text.textContent="ボタンをクリックしました"
+  setTimeout(() => {
+    text.textContent="ボタンをクリックしました"
+  },2000)
 });
